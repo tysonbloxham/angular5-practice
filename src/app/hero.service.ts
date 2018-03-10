35eaefbedd8f5a1532a6709a6b451220cedbb476
Tyson Bloxham <tysonsbloxham@gmail.com>
2018-03-09 18:52:44 -0700

    Turned hero service into an observable

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
+import { Observable } from "rxjs/Observable";
+import { of } from "rxjs/observable/of";
 import { Hero } from "./hero";
 import { HEROES } from "./mock-heroes";
 
 @Injectable()
 export class HeroService {
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+    return of(HEROES);
   }
 
   constructor() { }
