45a2143590736bd46f5e4605c47cd3aabd532bbf
Tyson Bloxham <tysonsbloxham@gmail.com>
2018-03-10 02:49:00 -0700

    Added ability to add new heroes from hero list

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -45,6 +45,13 @@ export class HeroService {
     );
   }
 
+  addHero (hero:Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, httpOptions).pipe(
+      tap((her: Hero) => this.log(`added hero w/ id=${hero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+
   private handleError<T> (operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
       console.error(error);
