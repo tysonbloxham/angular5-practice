807056fb9fe767ed074966c3387fe09b54d9f374
Tyson Bloxham <tysonsbloxham@gmail.com>
2018-03-09 19:08:58 -0700

    Injected message service into hero service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,6 +3,7 @@ import { Observable } from "rxjs/Observable";
 import { of } from "rxjs/observable/of";
 import { Hero } from "./hero";
 import { HEROES } from "./mock-heroes";
+import { MessageService } from "./message.service";
 
 @Injectable()
 export class HeroService {
@@ -10,6 +11,6 @@ export class HeroService {
     return of(HEROES);
   }
 
-  constructor() { }
+  constructor(private messageService) { }
 
 }
