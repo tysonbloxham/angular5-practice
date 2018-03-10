8c2cf767a2e73ef0dd5da7e6774dede0d330f079
Tyson Bloxham <tysonsbloxham@gmail.com>
2018-03-10 01:01:55 -0700

    Made a routable hero detail component by extracting the id from the url

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'deatil:id', component: HeroDetailComponent },
+  { path: 'detail/:id', component: HeroDetailComponent },
   { path: 'heroes', component: HeroesComponent }
 ];
 
