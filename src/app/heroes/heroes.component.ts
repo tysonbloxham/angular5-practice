da9f1b94d98cd18940e2a05c86de83f3e1e94787
Tyson Bloxham <tysonsbloxham@gmail.com>
2018-03-09 17:37:03 -0700

    Added event listener for hero click and selected hero variable

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,6 +11,12 @@ export class HeroesComponent implements OnInit {
 
   heroes = HEROES
 
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
